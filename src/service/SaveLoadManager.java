ddfb78f5dd62016ac9616a83f335876a76e8c02b
sbudagova17228 <budaqovasofi@gmail.com>
2026-04-19 23:27:08 +0400

    moved files

---
+++ src/service/SaveLoadManager.java
@@ -0,0 +1,13 @@
+package service; 
+/*it should be located in service folder, but just writing it here for now
+ */                 
+import java.util.Queue;
+import java.io.*;
+
+/**
+ * SaveLoadManager handles saving and loading
+ * the base and task queue using serialization.
+ */
+public class SaveLoadManager {
+    
+}
---
+++ src/service/TaskGenerator.java
@@ -0,0 +1,16 @@
+package service; //not in service folder on main branch yet but living it here for now
+import javafx.animation.KeyFrame;
+import javafx.animation.TimeLine;
+import javafx.util.duration;
+import model.*;
+import java.util.Queue;
+import java.util.Random;
+import java.util.function.Consumer;
+
+/* TaskGenerator automatically creates random tasks
+ * every few seconds and adds them to the task queue.
+ */
+
+public class TaskGenerator {
+
+}
