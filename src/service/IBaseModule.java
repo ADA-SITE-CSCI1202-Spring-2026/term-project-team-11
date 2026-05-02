070299c4f0ea344a7621e45c0cbc22b9ac7c911e
Mövlan <emenovmovlan@gmail.com>
2026-05-02 17:14:42 +0400

    Deleted IBaseModule.java file
    Because the file must but IProcessor.java

--- src/service/IBaseModule.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import model.BaseState;
-import model.Task;
-
-public interface IBaseModule {
-    boolean canProcess(Task task);
-    void processTask(Task task, BaseState state);
-}
